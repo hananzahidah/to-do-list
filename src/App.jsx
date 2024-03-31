e14a9d18466a14efe2311276d1a9c29c6618a813
hananzahidah <hananzah.05@gmail.com>
2024-04-01 05:04:18 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
+import { useState } from 'react'
 import TaskInput from './components/input'
 import ListItem from './components/itemList'
 
 function App() {
+  const [toDoList, setToDoList] = useState([]);
+
+  const addTask = (taskName) => {
+    const newTask = {taskName, checked: false};
+    setToDoList([...toDoList, newTask])
+  }
+  
+  function deleteTask(deleteTaskName) {
+    setToDoList(toDoList.filter(task => task.taskName !== deleteTaskName))
+  }
+
   return (
     <>
      <div className="w-3/4 mx-auto my-20 bg-red-100 p-2 rounded-xl lg:w-1/2 lg:p-4">
         <h1 className="text-center text-black font-bold uppercase my-4 text-2xl">To-Do List</h1>
 
-        <TaskInput/>
-        <ListItem/>
+        <TaskInput addTask={addTask}/>
+        {/* <ListItem/> */}
+
+        <div className="py-5">
+          <ul>
+            {toDoList.map((task, key) => (<ListItem task={task} key={key} deleteTask={deleteTask} />))}
+          </ul>
+        </div>
 
         <div className="text-center w-1/2 mx-auto bg-rose-200 p-2 rounded-xl font-semibold lg:w-40">
             <span className="p-1">Done: 0</span>
@@ -19,3 +37,4 @@ function App() {
 }
 
 export default App
+
--- src/components/itemList.jsx
+++ src/components/itemList.jsx
@@ -1,65 +1,19 @@
-
-function ListItem() {
-    return(
-      <div className="my-4 flex flex-col gap-1">
-        <ul>
-        <li className="flex justify-between items-center bg-red-50 p-2 rounded-lg">
-          <div className="flex gap-2 items-center">
-              <input type="checkbox"/>
-              <p className="text-black">task 1 task 1 task 1</p>
-          </div>
-          <div>
-          <svg xmlns="http://www.w3.org/2000/svg" className=" cursor-pointer hover:text-gray-600 h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-          </div>
-        </li>
-        </ul>
-
-        <ul>
-        <li className="flex justify-between items-center bg-red-50 p-2 rounded-lg">
-          <div className="flex gap-2 items-center">
-              <input type="checkbox"/>
-              <p className="text-black">task 2 task 2 task 2</p>
-          </div>
-          <div>
-          <svg xmlns="http://www.w3.org/2000/svg" className=" cursor-pointer hover:text-gray-600 h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-          </div>
-        </li>
-        </ul>
-
-        <ul>
-        <li className="flex justify-between items-center bg-red-50 p-2 rounded-lg">
-          <div className="flex gap-2 items-center">
-              <input type="checkbox"/>
-              <p className="text-black">task 3 task 3 task 3</p>
-          </div>
-          <div>
-          <svg xmlns="http://www.w3.org/2000/svg" className=" cursor-pointer hover:text-gray-600 h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-          </div>
-        </li>
-        </ul>
-
-        <ul>
-        <li className="flex justify-between items-center bg-red-50 p-2 rounded-lg">
-          <div className="flex gap-2 items-center">
-              <input type="checkbox"/>
-              <p className="text-black">task 4 task 4 task 4</p>
-          </div>
-          <div>
-          <svg xmlns="http://www.w3.org/2000/svg" className=" cursor-pointer hover:text-gray-600 h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-          </div>
-        </li>
-        </ul>
-
+function ListItem({task, deleteTask}){
+  return(
+    <>
+    <div className="pb-1">
+    <li className="flex justify-between items-center bg-red-50 p-2 rounded-lg ">
+      <div className="flex gap-3">
+        <input type="checkbox"/>
+        <p>{task.taskName} </p>
       </div>
-    )
+         <svg xmlns="http://www.w3.org/2000/svg" onClick={() => deleteTask(task.taskName)} className=" cursor-pointer hover:text-gray-600 h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+    </li>
+    </div>
+    </>
+  )
 }
 
 export default ListItem;
